ce755d9a51cc9c573e2acd66d88f094bf465939f
Leandro Aguirre <leandro.aguirre.m@gmail.com>
2024-05-22 14:35:39 -0300

    Fiz index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, GitHub Actions");
+// Importa o módulo express
+const express = require('express');
+
+// Cria uma instância do aplicativo express
+const app = express();
+
+// Define a porta na qual o servidor vai escutar
+const PORT = process.env.PORT || 3000;
+
+// Define uma rota para o caminho raiz ('/') que responde com 'Hello, World!'
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Faz o aplicativo escutar na porta especificada
+app.listen(PORT, () => {
+  console.log(Server is running on http://localhost:${PORT});
+});
